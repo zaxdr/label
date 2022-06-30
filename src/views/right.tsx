8677e41eb4293273d6605cac074ccd19b5fdd042
guanglin <guanglin.zhang@dorabot.com>
2022-06-30 18:49:11 +0800

    手动生成条码数据

--- src/views/right.tsx
+++ src/views/right.tsx
@@ -3,8 +3,8 @@ import VisibleNode from "@/components/comm/VisibleNode";
 import { DataType, DataTypeList } from "@/model/options";
 import IStore from "@/model/StoresModel";
 import { setDataType } from "@/stores/pageStore";
-import { useCreatePDF } from "@/utils/hooks";
-import { createList, printPdf, returnEndValue, returnTotal } from "@/utils/labelUtils";
+import { useCreatePDF,customCreatePDF } from "@/utils/hooks";
+import { createList, printPdf, createListV2 } from "@/utils/labelUtils";
 import { Button, Card, Input, InputNumber, message, Select, Switch, Tag } from "antd";
 import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
@@ -13,16 +13,14 @@ const TextArea = Input.TextArea;
 
 
 const RightComponent = () => {
-    const { dataType, } = useSelector((store: IStore) => store.pageStore);
-    const { create } = useCreatePDF()
-
+    const { codeData, paperData, isContent, contentData, qrType ,dataType} = useSelector((store: IStore) => store.pageStore);
+    const { create } = useCreatePDF({codeData, paperData, isContent, contentData, qrType})
     const [start, setStart] = useState<string>("A100000001");
-    const [end, setEnd] = useState<string>("A100000001");
     const [total, setTotal] = useState<number>(1);
     const [isStep, setIsStep] = useState<boolean>(false);
-    const [fixedValue, setFixedValue] = useState<string>("A100000001\nA100000002");
+    const [fixedValue, setFixedValue] = useState<string>("A100000001\nA100000002");//初始化固定值
+    const [fileName,setFileName] = useState<string>(`label-${new Date().getTime()}`) // 文件名称
     const [num, setNum] = useState<number>(1);
-
     const dispatch = useDispatch();
     const [printList, setPrintList] = useState<string[]>([]);
     //修改类型
@@ -30,91 +28,54 @@ const RightComponent = () => {
         dispatch(setDataType(value))
     }, [dispatch])
 
-    //输入是否递增
-    const inputStep = useCallback((bool: boolean) => {
-        setIsStep(bool);
-
-    }, [])
-
-
-    //输入开始值(输入完开始值 根据总数 重置结束值)
-    const inputStart = useCallback((e: string) => {
-        setStart(e);
-        setEnd(returnEndValue(e, total));
-    }, [total])
-
-    //输入结束值
-    const inputEnd = useCallback((v: string) => {
-        if (!start) {
-            message.error("请输入开始值");
-            return;
-        }
-        setEnd(v);
-        setTotal(returnTotal(start, v))
-    }, [start])
-
-    //输入总数
-    const inputTotal = useCallback((e: number) => {
-        setTotal(e);
-        if (!start) {
-            message.error("请输入开始值");
-            return;
-        }
-
-        let endValue = returnEndValue(start, e)
-        setEnd(endValue);
-    }, [start])
-
-    //输入个数
-    const inputNum = useCallback((e: number) => {
-        if (!start) {
-            message.error("请输入开始值");
-            return;
-        }
-        setNum(e)
-    }, [start, total])
-
-
-    useEffect(() => {
+    const getPrintList = useCallback(()=>{
         let list: string[] = [];
         if (dataType === "Auto") {
-            if (isStep) {
-                //递增
-                list = createList(start, end, num, total);
-            } else {
-                //默认
-                list = createList(start, start, num, total);
-            }
+            list =   createListV2(start,num,total,isStep)
         } else {
             list = fixedValue.trim().split("\n");//去除前后空格 然后按换行分割
         }
-        setPrintList(list);
-
-    }, [start, end, num, isStep, total, dataType, fixedValue])
+        return list;
+    },[printList,fixedValue,dataType,start,num,total,isStep])
 
 
     //打印
-    const print = useCallback(() => {
-        if (printList.length === 0) {
+    const print = () => {
+        let list = getPrintList();
+        if (list.length === 0) {
             message.error("请输入需要生成的数据")
             return;
         }
-        create(printList, (pdf) => {
+        customCreatePDF(list,(pdf)=>{
             let st: any = pdf.output("bloburl")
             printPdf(st)
+        },{
+            codeData, paperData, isContent, contentData, qrType ,dataType
         })
-    }, [printList])
+    }
 
     //下载
-    const dowload = useCallback(() => {
-        if (printList.length === 0) {
+    const dowload = () => {
+        let list = getPrintList();
+        if (list.length === 0) {
             message.error("请输入需要生成的数据")
             return;
         }
-        create(printList, (pdf) => {
-            pdf.save("label.pdf")
+        customCreatePDF(list,(pdf)=>{
+            pdf.save(`${fileName}.pdf`)
+        },{
+            codeData, paperData, isContent, contentData, qrType ,dataType
         })
-    }, [printList])
+    }
+
+    //生成数据
+    const createReview = useCallback(()=>{
+        if (!start) {
+            message.error("请输入开始值");
+            return;
+        }
+        setPrintList(getPrintList());
+    },[start,getPrintList])
 
 
     return <Card title="批量生成" size="small">
@@ -135,28 +96,27 @@ const RightComponent = () => {
         </VisibleNode>
         <VisibleNode visible={dataType === "Auto"}>
             <LabelItem title="是否递增">
-                <Switch checked={isStep} checkedChildren="是" unCheckedChildren="否" onChange={inputStep} />
+                <Switch checked={isStep} checkedChildren="是" unCheckedChildren="否" onChange={setIsStep} />
             </LabelItem>
             <LabelItem title="开始值">
-                <Input value={start} onChange={e => inputStart(e.target.value)} />
+                <Input value={start} onChange={e => setStart(e.target.value)} />
             </LabelItem>
-            <VisibleNode visible={isStep}>
-                <LabelItem title="结束值">
-                    <Input value={end} onChange={e => inputEnd(e.target.value)} />
-                </LabelItem>
-            </VisibleNode>
             <LabelItem title="总数">
-                <InputNumber value={total} min={0} onChange={inputTotal} />
+                <InputNumber value={total} min={0} onChange={(e)=>setTotal(e)} />
             </LabelItem>
             <LabelItem title="单个倍数">
-                <InputNumber value={num} min={1} onChange={inputNum} />
+                <InputNumber value={num} min={1} onChange={(e)=>setNum(e)} />
                 <Tag color="lime">{total * num}</Tag>
             </LabelItem>
             <LabelItem title="预览">
+                <Button onClick={createReview}>生成数据</Button>
                 <TextArea rows={10} disabled value={printList.join('\n')} />
             </LabelItem>
         </VisibleNode>
         <div className="btns">
+            <LabelItem title="文件名称">
+                <Input value={fileName} onChange={e=>setFileName(e.target.value)} />
+            </LabelItem>
             <Button type="primary" onClick={print}>打印</Button>
             <Button type="primary" onClick={dowload}>下载</Button>
         </div>
